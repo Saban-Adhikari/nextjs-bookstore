440695ebd84787bebe0869fd11709de245fc1db1
Saban-Adhikari <101613059+Saban-Adhikari@users.noreply.github.com>
2024-05-07 09:58:05 +0545

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
         <SpeedInsights />
+        <Analytics />
         <Footer />
       </body>
     </html>
